c226f26cd2688cd437b05d35f2867a0370caa227
Jabgomez <josebaquerizo_1999@hotmail.com>
2023-02-21 23:33:20 -0500

    Refactor sales by match challenge

--- src/sales-by-match/index.ts
+++ src/sales-by-match/index.ts
@@ -6,7 +6,7 @@ export function sockMerchant(n: number, ar: number[]): number {
 		let count = 0;
 
 		for(let j=0; j < ar.length; j++) {
-			if(ar[i] == ar[j]) { count += 1; continue; }
+			if(ar[i] === ar[j]) { count += 1; continue; }
 			filteredAr.push(ar[j]);
 		}
 
