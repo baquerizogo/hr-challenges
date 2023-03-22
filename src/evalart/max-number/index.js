4ec3749520a254edc44cd3373cadd8df96d2bafe
Jabgomez <josebaquerizo_1999@hotmail.com>
2023-03-22 09:35:21 -0500

    Add second solution to find max number

--- src/evalart/max-number/index.js
+++ src/evalart/max-number/index.js
@@ -4,3 +4,16 @@ export const maxNumber = (arr) => {
 	
 	return Math.max(...arr);
 };
+
+export const maxNumber2 = (arr) => {
+	let max = 0;
+
+	if(!arr) throw new Error("arr is mandatory");
+	if(!Array.isArray(arr)) throw new Error("arr must be type Array");
+
+	arr.map(e => {
+		if(e > max) max = e;
+	});
+
+	return max;
+};
--- src/evalart/max-number/index.test.js
+++ src/evalart/max-number/index.test.js
@@ -1,4 +1,4 @@
-import { maxNumber } from ".";
+import { maxNumber, maxNumber2 } from ".";
 
 describe("maxNumber", () => {
 	it("should return a number", () => {
@@ -21,6 +21,32 @@ describe("maxNumber", () => {
 
 	it("should return the max number (25) of the array even if the number is repeated", () => {
 		const res = maxNumber([10, 25, 3, 25, 4]);
+		expect(res).toBe(25);
+	});
+});
+
+
+describe("maxNumber2", () => {
+	it("should return a number", () => {
+		const res = maxNumber2([0]);
+		expect(typeof res).toBe("number");
+	});
+
+	it("should throw error if there is no parameters", () => {
+		expect(maxNumber2).toThrow();
+	});
+
+	it("should throw error if there the parameter is not an array", () => {
+		expect(() => maxNumber2(2)).toThrow();
+	});
+
+	it("should return the max number (87) of the array", () => {
+		const res = maxNumber2([10, 2, 3, 87, 4]);
+		expect(res).toBe(87);
+	});
+
+	it("should return the max number (25) of the array even if the number is repeated", () => {
+		const res = maxNumber2([10, 25, 3, 25, 4]);
 		expect(res).toBe(25);
 	});
 });
